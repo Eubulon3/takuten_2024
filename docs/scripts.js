50c1a2d7941c0c52c208dbcae9d652e24f65bdfe
chashu <chashu@chashus-MacBook-Pro.local>
2024-07-19 16:51:23 +0900

    nav-box調整

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuIcon = document.getElementById("menu-icon");
-    const navbar = document.getElementById("navbar");
+    const navbox = document.getElementById("nav-box");
     const header = document.getElementById("header");
 
     menuIcon.addEventListener("click", function() {
-        navbar.classList.toggle("show");
+        navbox.classList.toggle("show");
         header.classList.toggle("hide");
-        if (navbar.classList.contains('show')) {
+        if (navbox.classList.contains('show')) {
             document.body.style.overflow = 'hidden'; // スクロールを許可
         } else {
             document.body.style.overflow = 'auto'; // スクロールを禁止
