ed05cfa6447534c01938ba4e345170e25ce80857
chashu <chashu@chashus-MacBook-Pro.local>
2024-07-24 19:43:45 +0900

    フォント微調整

--- docs/scripts.js
+++ docs/scripts.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const menuIcon = document.getElementById("menu-icon");
     const navbox = document.getElementById("nav-box");
     const header = document.getElementById("header");
+    const links = navbox.getElementsByTagName("a");
 
     menuIcon.addEventListener("click", function() {
         navbox.classList.toggle("show");
@@ -12,4 +13,16 @@ document.addEventListener("DOMContentLoaded", function() {
             document.body.style.overflow = 'auto'; // スクロールを禁止
         }
     });
+
+    for (let link of links){
+        link.addEventListener("click", () => {
+            navbox.classList.toggle("show");
+            header.classList.toggle("hide");
+            if (navbox.classList.contains('show')) {
+                document.body.style.overflow = 'hidden'; // スクロールを許可
+            } else {
+                document.body.style.overflow = 'auto'; // スクロールを禁止
+            }
+        });
+    }
 });
